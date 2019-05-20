d0ac80caf045d662ff7b2f099ef74ca43254d3c4
alexs55 <alex.acosta.dv@gmail.com>
2019-05-20 18:53:21 -0500

    workign initial game loop

--- test/testlogic/testingcode.js
+++ test/testlogic/testingcode.js
@@ -1,26 +1,38 @@
-var chefSkill = {"burger":1.5,"hotdog":1};
-var time = 120;
-var orders = ["burger","hot-dog","hot-dog","hot-dog","hot-dog","hot-dog","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger"];
+var chefSkill = {"burger":2.5,"hotdog":1.5};
+var orders = ["burger","hot-dog","hot-dog","hot-dog","hot-dog","hot-dog","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","hot-dog","hot-dog","hot-dog","hot-dog","hot-dog","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","hot-dog","hot-dog","hot-dog","hot-dog","hot-dog","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","hot-dog","hot-dog","hot-dog","hot-dog","hot-dog","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","hot-dog","hot-dog","hot-dog","hot-dog","hot-dog","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","hot-dog","hot-dog","hot-dog","hot-dog","hot-dog","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","hot-dog","hot-dog","hot-dog","hot-dog","hot-dog","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger","burger"];
+let time = 720;
+// we can set the time to the random variable when choosing the random chance issue that could appear and add it to the time that goes inside of the loop, this could be a variable like randomTime
+
+
 
 function DayGoesBy(chefSkill,time,orders){
+    time = 720;
     let randomOrder = orders.sort(function(){
         return 0.5 - Math.random();
     })
-    for ( let i = 0; i< randomOrder.length; i++){
-        let orderA = 0;
-        let orderB = 0;
-        if (randomOrder[i]=== "burger" && time > 0){
+    let orderA = 0;
+    let orderB = 0;
+for ( let i = 0; i< randomOrder.length; i++){
+        
+    while(time > 0){
+        if (randomOrder[i] === "burger"){
             time -= chefSkill.burger
             orderA++
-           
-        }if(randomOrder[i]=== "hot-dog" && time > 0){
+           console.log(time);
+
+        }
+        if(randomOrder[i] === "hot-dog"){
             time -= chefSkill.hotdog
             orderB++
-            
-        }else{
-            DayOver(orderA,orderB);
+            console.log(time);
+
         }
     }
+    while(time <= 0){
+            DayOver(orderA,orderB);
+            return;
+    }
+}
 }
 
 function DayOver(A,B){
