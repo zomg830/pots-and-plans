72e78a18ccada72733b0507ce6ac032e67d11d5c
alexs55 <alex.acosta.dv@gmail.com>
2019-05-29 17:21:31 -0500

    the urls for the gifs/pictures are working now

--- client/src/utils/randomEvent.js
+++ client/src/utils/randomEvent.js
@@ -63,7 +63,7 @@ export default function randomEvent() {
       result.time = 0;
       result.message =
         "Your cook didn't sleep a wink last night.  Cooking Skill decreases.";
-      result.imageURL = "./images/sleepy.gif";
+      result.imageURL = "https://github.com/zomg830/pots-and-plans/blob/aadvbranch/client/src/images/sleepy.gif?raw=true";
       break;
     case "plumbing":
       result.balance -= 1050;
@@ -71,7 +71,7 @@ export default function randomEvent() {
       result.time = 0;
       result.message =
         "Oh no, the water main burst!!  Pay the plumber $1050.00.";
-      result.imageURL = "./images/plumber.png";
+      result.imageURL = "https://github.com/zomg830/pots-and-plans/blob/aadvbranch/client/src/images/plumber.png?raw=true";
       break;
     case "convention":
       result.time += 150;
@@ -102,7 +102,7 @@ export default function randomEvent() {
       result.skill = 0;
       result.time = 0;
       result.message = "Bad health inspection.  Give inspector $1000.00 bribe.";
-      result.imageURL = "./images/inspection.png";
+      result.imageURL = "https://github.com/zomg830/pots-and-plans/blob/aadvbranch/client/src/images/inspection.png?raw=true";
       break;
     case "competition":
       result.time += 150;
